c2083cdea9046a81e283877a828a804c90675440
gregorule <gregorule8@gmail.com>
2022-06-10 09:52:28 +0000

    debug commit

---
+++ debug.py
@@ -0,0 +1,20 @@
+import pdb
+
+pdb.set_trace()
+'''
+a = "aaa"
+b = "bbb"
+c = "ccc"
+
+final = a + b + c
+print(final)
+'''
+user_funds = 10.31
+item_price = price["Burger"]
+
+if item_price < user_funds:
+    print("You have enough money!")
+elif item_price == user_funds:
+    print("You have the precise amount of money")
+else item_price > user_funds:
+    print("Sorry you don't have enough money")
