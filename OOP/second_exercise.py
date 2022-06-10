4b2c4ebab4bd1ce5ce5bed5513c20975985aacf6
gregorule <gregorule8@gmail.com>
2022-06-10 14:09:36 +0000

    OOP revised commit 3

---
+++ OOP/second_exercise.py
@@ -0,0 +1,8 @@
+
+books = {"JK Rowling": ["Philosopher's Stone", "Chamber of Secrets", "Prisoner of Azkaban", "Goblet of Fire", "Order of the Phoenix", "Half-Blood Prince", "Deathly Hallows"], "Dan Brown": ["Angels & Demons", "The Da Vinci Code", "The Lost Symbol", "Inferno", "Origin"], "Stephen King": ["IT", "The Shining", "The Stand", "Carrie", "The Dead Zone", "Cujo", "The Long Walk"]}
+
+print(books["JK Rowling"])
+
+books["George Orwell"] = "1984", "Animal Farm", "Homage to Catelonia", "Down and Out in Paris and in London"
+
+print(books)
